b62baa2f37bda25db1a3fdb211710236d54f98f6
DHARUN RAJ P <pdharunraj@gmail.com>
2026-09-11 10:35:32 +0530

    Implement MaxMin program to find max and min values

---
+++ Practice/MaxMin.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <limits.h>
+int main() {
+    int n;
+    scanf("%d",&n);
+    int arr[n];
+    for(int i=0;i<n;i++){
+        scanf("%d",&arr[i]);
+    }
+    int max=INT_MIN;
+    int min=INT_MAX;
+    for(int i=0;i<n;i++){
+        if(arr[i]>max){
+            max=arr[i];
+        }
+        if(arr[i]<min){
+            min=arr[i];
+        }
+    }
+    printf("Maximum is : %d\n",max);
+    printf("Minimum is : %d",min);
+}
